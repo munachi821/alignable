2b0dc1a75c20eb602c9d7f6d4ba52fc141611d82
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-11-07 22:17:02 +0100

    done with the search feature and the navbar

---
+++ components/SVG/EventSVG.tsx
@@ -0,0 +1,15 @@
+const EventSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      role="img"
+      height="1em"
+      fill="currentColor"
+      className="mr-2.5 h-5 w-5 shrink-0 text-[#8b95a3]"
+    >
+      <path d="M19 4h-1V3c0-.55-.45-1-1-1s-1 .45-1 1v1H8V3c0-.55-.45-1-1-1s-1 .45-1 1v1H5c-1.11 0-1.99.9-1.99 2L3 20a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2Zm-1 16H6c-.55 0-1-.45-1-1V9h14v10c0 .55-.45 1-1 1Z"></path>
+    </svg>
+  );
+};
+export default EventSVG;
--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -15,23 +15,83 @@ import SupportSVG from "../SVG/SupportSVG";
 import InsightsSVG from "../SVG/InsightsSVG";
 import AccountSVG from "../SVG/AccountSVG";
 import { useState } from "react";
+import BusinessSVG from "../SVG/BusinessSVG";
+import ServicesSVG from "../SVG/ServicesSVG";
+import EventSVG from "../SVG/EventSVG";
+import HelpSVG from "../SVG/HelpSVG";
+import GroupsSVG from "../SVG/GroupsSVG";
 
 const Navbar = () => {
   const [openProfile, setOpenProfile] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const modalSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value.trim());
+  };
   return (
     <header>
       <nav className="w-full bg-[#391590] ">
         <div className="flex text-white py-0.5 items-center justify-between max-w-[1140px] mx-auto">
-          <div className="flex h-fit gap-2 relative">
+          <div className="flex h-fit gap-2 relative group">
             <AllignableSVG />
             <input
               type="search"
               name="search"
               id="search"
+              onChange={modalSearch}
               placeholder="Search Alignable"
               className="w-56 bg-white text-black outline-0 rounded-md pl-8 placeholder:font-semibold placeholder:text-[#a9afb6]"
             />
             <SearchSVG />
+
+            <div className="absolute top-12 left-11.5 w-[318px] bg-white text-black p-3 rounded-xl shadow-md hidden group-focus-within:block overflow-x-hidden">
+              <p className="text-[#8b95a3] font-semibold text-sm">
+                SEARCH FOR...
+              </p>
+              <div>
+                <div className="mt-2 flex items-center font-semibold hover:bg-[#f3f5f8] h-9 transition-colors pl-1 rounded-md cursor-pointer">
+                  <BusinessSVG />
+                  <span className="whitespace-nowrap truncate">
+                    {searchInput}
+                  </span>
+                  <p className="ml-1">businesses</p>
+                </div>
+                <div className="flex items-center font-semibold hover:bg-[#f3f5f8] transition-colors pl-1 h-9 rounded-md cursor-pointer">
+                  <ServicesSVG />
+                  <span className="whitespace-nowrap truncate text-[#1f2937]">
+                    {searchInput}
+                  </span>
+                  <p className="ml-1">services</p>
+                </div>
+                <div className="flex items-center font-semibold hover:bg-[#f3f5f8] transition-colors pl-1 h-9 rounded-md cursor-pointer">
+                  <EventSVG />
+                  <span className="whitespace-nowrap truncate text-[#1f2937]">
+                    {searchInput}
+                  </span>
+                  <p className="ml-1">events</p>
+                </div>
+                <div className="flex items-center font-semibold hover:bg-[#f3f5f8] transition-colors pl-1 h-9 rounded-md cursor-pointer">
+                  <GroupsSVG />
+                  <span className="whitespace-nowrap truncate text-[#1f2937]">
+                    {searchInput}
+                  </span>
+                  <p className="ml-1">group</p>
+                </div>
+                <div className="flex items-center font-semibold hover:bg-[#f3f5f8] transition-colors pl-1 h-9 rounded-md cursor-pointer">
+                  <HelpSVG />
+                  <span className="whitespace-nowrap truncate text-[#1f2937]">
+                    {searchInput}
+                  </span>
+                  <p className="ml-1">help</p>
+                </div>
+                <div className="font-semibold flex items-center text-[#5023b0] hover:bg-[#f3f5f8] transition-colors pl-1 h-9 rounded-md mt-2">
+                  <p className="whitespace-nowrap truncate">
+                    All results for &quot;
+                    <span>{searchInput}</span>
+                    &quot;
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
 
           <ul className="flex">
@@ -106,7 +166,7 @@ const Navbar = () => {
                           <VerifiedSVG />
                           <p className="font-semibold">Become Premium</p>
                         </div>
-                        <span className="text-[#7f5026] bg-[#fbedd6] font-bold leading-relaxed text-[12px] p-0.5 px-1.5 rounded-md">
+                        <span className="text-[#7f5026] bg-[#fbedd6] font-bold text-[12px] p-0.5 px-1.5 rounded-md">
                           UPGRADE
                         </span>
                       </div>
