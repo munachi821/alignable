b10f2918987217759b94fddaf5060b38aa3d8ee1
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-11-07 23:54:49 +0100

    still doing the feed

--- app/(dashboard)/biz/page.tsx
+++ app/(dashboard)/biz/page.tsx
@@ -27,8 +27,28 @@ const page = () => {
         </a>
       </div>
 
-      <div className="flex gap-7.5">
-        <div className="max-w-[275px]"></div>
+      <div className="flex gap-7.5 max-w-[1140px] mx-auto">
+        <div className="max-w-[275px] w-full">
+          <div className="flex p-3 items-center gap-3 rounded-lg mb-1">
+            <span className="size-11 rounded-full flex items-center justify-center bg-[#f9f0ff] text-[#391590] border border-[#391590] text-sm font-semibold">
+              CM
+            </span>
+            <ul className="leading-5">
+              <li className="font-bold">Munachi Onyebuchi</li>
+              <li className="font-semibold text-sm">Hepdex</li>
+            </ul>
+          </div>
+
+          <div className="border border-[#e2e8f0] p-2 bg-[#f8fafc] rounded-md">
+            <div className="w-full h-2 rounded-full bg-[#c5ced9] overflow-hidden">
+              <div className="w-[18%] h-full bg-[#6c33d8] rounded-full"></div>
+            </div>
+            <p className="text-[#6c33d8] mt-2 text-sm font-bold">
+              Your profile is 18% refferal ready
+            </p>
+          </div>
+        </div>
+
         <div className="max-w-[805px]"></div>
       </div>
     </section>
