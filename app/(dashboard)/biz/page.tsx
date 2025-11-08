643d0135544c3899f5300765df76853265aa6752
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-11-08 14:07:47 +0100

    the feed

--- app/(dashboard)/biz/page.tsx
+++ app/(dashboard)/biz/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import Image from "next/image";
 import rocketImg from "../../../public/images/rocket_jumpstart_logo.png";
-const page = () => {
+import ViewsSVG from "@/components/SVG/ViewsSVG";
+import ClickSVG from "@/components/SVG/ClickSVG";
+import InfoSVG from "@/components/SVG/InfoSVG";
+import { useState } from "react";
+const Page = () => {
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <section className="w-screen min-h-screen py-8">
       <div className="max-w-[1150px] mx-auto bg-[#2d3748] px-4 py-4 rounded-xl flex justify-between items-center">
@@ -27,31 +33,58 @@ const page = () => {
         </a>
       </div>
 
-      <div className="flex gap-7.5 max-w-[1140px] mx-auto">
+      <div className="flex gap-7.5 max-w-[1140px] mx-auto mt-5">
         <div className="max-w-[275px] w-full">
-          <div className="flex p-3 items-center gap-3 rounded-lg mb-1">
-            <span className="size-11 rounded-full flex items-center justify-center bg-[#f9f0ff] text-[#391590] border border-[#391590] text-sm font-semibold">
-              CM
-            </span>
-            <ul className="leading-5">
-              <li className="font-bold">Munachi Onyebuchi</li>
-              <li className="font-semibold text-sm">Hepdex</li>
-            </ul>
-          </div>
-
-          <div className="border border-[#e2e8f0] p-2 bg-[#f8fafc] rounded-md">
-            <div className="w-full h-2 rounded-full bg-[#c5ced9] overflow-hidden">
-              <div className="w-[18%] h-full bg-[#6c33d8] rounded-full"></div>
+          <div className="border-b pb-5.5 border-[#d3d3d3]">
+            <div className="flex p-3 items-center gap-3 rounded-lg mb-1">
+              <span className="size-11 rounded-full flex items-center justify-center bg-[#f9f0ff] text-[#391590] border border-[#391590] text-sm font-semibold">
+                CM
+              </span>
+              <ul className="leading-5">
+                <li className="font-bold">Munachi Onyebuchi</li>
+                <li className="font-semibold text-sm">Hepdex</li>
+              </ul>
             </div>
-            <p className="text-[#6c33d8] mt-2 text-sm font-bold">
-              Your profile is 18% refferal ready
-            </p>
+
+            <div className="border border-[#e2e8f0] p-2 bg-[#f8fafc] rounded-md">
+              <div className="w-full h-2 rounded-full bg-[#c5ced9] overflow-hidden">
+                <div className="w-[18%] h-full bg-[#6c33d8] rounded-full"></div>
+              </div>
+              <a
+                href="#"
+                className="text-[#6c33d8] mt-2 text-sm font-bold hover:underline"
+              >
+                Your profile is 18% refferal ready
+              </a>
+            </div>
+
+            <div className="flex items-center justify-between mt-8 relative">
+              <div className="text-[#6c33d8] font-bold flex gap-2 items-center text-sm group">
+                <ViewsSVG />{" "}
+                <a href="#" className="group-hover:underline">
+                  8 Views
+                </a>
+              </div>
+              <div className="text-[#6c33d8] font-bold flex gap-2 items-center text-sm group">
+                <ClickSVG />{" "}
+                <a href="#" className="group-hover:underline">
+                  3 Clicks
+                </a>
+              </div>
+              <div className="text-[#616b79] p-1 rounded-full cursor-pointer hover:bg-[#e2e8f0] transition-colors group">
+                <InfoSVG />
+                <div className="absolute w-150 bg-[#2d3748] p-4 -left-6.5 bottom-10 font-bold text-white rounded-lg hidden group-hover:block">
+                  Total views and clicks on your business profile, content and
+                  services in the last 90 days.
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="max-w-[805px]"></div>
+        <div className="max-w-[805px] w-full bg-white rounded-md shadow-md"></div>
       </div>
     </section>
   );
 };
-export default page;
+export default Page;
---
+++ components/SVG/ClickSVG.tsx
@@ -0,0 +1,16 @@
+const ClickSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      role="img"
+      width="21"
+      height="18"
+      fill="currentColor"
+    >
+      <title id="aca1wz5pq52ts59sxjxll3b1epj1bp8p"></title>
+      <path d="m14 21.9-3.7-7.925L6 20V2l14 11h-7.375l3.625 7.85L14 21.9Z"></path>
+    </svg>
+  );
+};
+export default ClickSVG;
